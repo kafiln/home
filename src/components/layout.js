e9aadfc1a9f18b8af1373f72bb568a974888cd2e
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-04-24 00:16:57 +0000

    Adding tailwindcss and styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,11 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import styled from "styled-components"
+const Wrapper = styled.div`
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,20 +30,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Wrapper className="bg-teal-300 mx-auto rounded-md p-4">
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Wrapper>
     </>
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const About = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>My Name</h1>
+    <h2>Welcome to My blog</h2>
+    <p>Some really funny things I have to say about me</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default About
